5573d2c94f1665aa5a9308fb98231ef7012842d4
Alex Lanz <alex.lanz@aboutbits.it>
2021-04-28 15:01:52 +0200

    Fixing wrong prod url

--- infrastructure/Jenkinsfile-Prod.groovy
+++ infrastructure/Jenkinsfile-Prod.groovy
@@ -9,7 +9,7 @@ pipeline {
         SERVER_PORT = "1012"
 
         URL_BASE = "https://destinationdata.alpinebits.opendatahub.bz.it"
-        URL_VERSION_1_0 = "http://docker02.testingmachine.eu:1003"
+        URL_VERSION_1_0 = "http://docker02.opendatahub.bz.it:1003"
     }
 
     stages {
