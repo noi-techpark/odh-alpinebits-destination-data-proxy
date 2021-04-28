a85543396c2a61d4dd82c86441878b109c100159
Alex Lanz <alex.lanz@aboutbits.it>
2021-04-28 11:05:27 +0200

    Adjusting test urls

--- infrastructure/Jenkinsfile-Test.groovy
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -9,7 +9,7 @@ pipeline {
         SERVER_PORT = "1012"
 
         URL_BASE = "https://destinationdata.alpinebits.opendatahub.testingmachine.eu"
-        URL_VERSION_1_0 = "https://1-0.destinationdata.alpinebits.opendatahub.testingmachine.eu"
+        URL_VERSION_1_0 = "http:/docker02.testingmachine.eu:1003"
     }
 
     stages {
