7f6f7ec9fab555826708c4a666276f9988f61943
Alex Lanz <alex.lanz@aboutbits.it>
2021-06-09 13:03:34 +0200

    Swagger specification deployment

--- infrastructure/Jenkinsfile-Prod.groovy
+++ infrastructure/Jenkinsfile-Prod.groovy
@@ -8,6 +8,7 @@ pipeline {
         
         SERVER_PORT = "1012"
 
+        URL_SWAGGER = "https://swagger.opendatahub.bz.it/?url=https://destinationdata.alpinebits.opendatahub.bz.it/specification.json"
         URL_BASE = "https://destinationdata.alpinebits.opendatahub.bz.it"
         URL_VERSION_1_0 = "http://docker04.opendatahub.bz.it:1003"
         URL_VERSION_2021_04 = "http://docker04.opendatahub.bz.it:1006"
@@ -25,6 +26,7 @@ pipeline {
 
                     echo 'SERVER_PORT=${SERVER_PORT}' >> .env
 
+                    echo 'URL_SWAGGER=${URL_SWAGGER}' >> .env
                     echo 'URL_BASE=${URL_BASE}' >> .env
                     echo 'URL_VERSION_1_0=${URL_VERSION_1_0}' >> .env
                     echo 'URL_VERSION_2021_04=${URL_VERSION_2021_04}' >> .env
@@ -34,6 +36,9 @@ pipeline {
         stage('Build') {
             steps {
                 sh '''
+                    (docker run --rm -v ${PWD}:/code -w /code/openapi node:16 npm install)
+                    (docker run --rm -v ${PWD}:/code -w /code/openapi node:16 npm run bundle)
+
                     aws ecr get-login --region eu-west-1 --no-include-email | bash
                     docker-compose --no-ansi -f infrastructure/docker-compose.build.yml build --pull
                     docker-compose --no-ansi -f infrastructure/docker-compose.build.yml push
